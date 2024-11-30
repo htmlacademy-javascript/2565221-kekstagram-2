3fd0139f57aba8131193f157ea328d694e5f16d4
Егор Елин <elin.zhora.05@gmail.com>
2024-11-30 22:25:09 +0500

    Добавляет функции палиндром, длина строки

--- js/functions.js
+++ js/functions.js
@@ -1,37 +1,16 @@
-const checkStringLength = (string = "", length = 1) => {
-  while (string <= length) {
-    return true
-  }
+const checkStringLength = (string = '', maxLength = 1) => {
+  const str = string.length <= maxLength;
+  return str;
 };
-checkStringLength();
-console.log("проверяемая строка", 20, checkStringLength("проверяемая строка", 20));
-console.log("проверяемая строка", 18, checkStringLength("проверяемая строка", 20));
-console.log("проверяемая строка", 10, checkStringLength("проверяемая строка", 20));
+checkStringLength('проверяемая строка', 20);
 
 const checkPalindrome = (string) => {
   string = string.toUpperCase();
-  string = string.replaceAll(" ", "");
-  let rewrittenString = "";
+  string = string.replaceAll(' ', '');
+  let rewrittenString = '';
   for (let i = string.length - 1; i >= 0; i--) {
-    rewrittenString += string[i]
-  };
-  return string === rewrittenString ? "Является палиндромом" : "Не является палиндромом";
-}
-// Tests palindrome
-console.log('\n--- Тесты для palindromeChecker ---');
-console.log(
-  `Строка является палиндромом: ${checkPalindrome('топот') === true}`
-);
-console.log(
-  `Строка является палиндромом с разным регистром: ${
-    checkPalindrome('ДовОд') === true
-  }`
-);
-console.log(
-  `Строка не является палиндромом: ${checkPalindrome('Кекс') === false}`
-);
-console.log(
-  `Строка является палиндромом: ${
-    checkPalindrome('Лёша на полке клопа нашёл ') === true
-  }`
-);
+    rewrittenString = rewrittenString + string[i];
+  }
+  return string === rewrittenString;
+};
+checkPalindrome('топот');
